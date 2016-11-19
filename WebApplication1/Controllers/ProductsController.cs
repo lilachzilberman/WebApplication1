1127bb7ed0839468797ba833490f38fcee6094d6
Lilach Zilberman <lilachavni@hotmail.com>
2016-11-19 10:55:48 +0200

    controller reqired admin login to alter branches and products

--- WebApplication1/Controllers/ProductsController.cs
+++ WebApplication1/Controllers/ProductsController.cs
@@ -36,6 +36,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET: Products/Create
+        [Authorize(Roles = "Admin")]
         public ActionResult Create()
         {
             return View();
@@ -46,6 +47,7 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public ActionResult Create([Bind(Include = "Id,Name,Price,Supplier,Animal,Category,PicturePath")] Products products)
         {
             if (ModelState.IsValid)
@@ -59,6 +61,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET: Products/Edit/5
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -78,6 +81,7 @@ namespace WebApplication1.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public ActionResult Edit([Bind(Include = "Id,Name,Price,Supplier,Animal,Category,PicturePath")] Products products)
         {
             if (ModelState.IsValid)
@@ -90,6 +94,7 @@ namespace WebApplication1.Controllers
         }
 
         // GET: Products/Delete/5
+        [Authorize(Roles = "Admin")]
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -107,6 +112,7 @@ namespace WebApplication1.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public ActionResult DeleteConfirmed(int id)
         {
             Products products = db.Products.Find(id);
@@ -115,6 +121,7 @@ namespace WebApplication1.Controllers
             return RedirectToAction("Index");
         }
 
+        [Authorize(Roles = "Admin")]
         protected override void Dispose(bool disposing)
         {
             if (disposing)
